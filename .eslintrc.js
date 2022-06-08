96be6337b6eade71061169ba08841a994d80c394
Dinert <1293437362@qq.com>
2022-06-08 21:02:46 +0800

    优化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,58 +4,69 @@ module.exports = {
     * 为了将ESLint限制在一个特定的项目，设置root: true；
     * ESLint一旦发现配置文件中有 root: true，就会停止在父级目录中寻找。
     */
-   root: true,
-   // 指定脚本运行环境
-   env: {
-     browser: true,
-     node: true
-   },
-   // 基础配置
-   'extends': [
-     '@vue/standard',
-     'plugin:vue/recommended'
-   ],
-   // 指定解析器
-   // babel-eslint：一个对babel解析器的包装，使其能够参与Eslint兼容
-   parserOptions: {
-     parser: 'babel-eslint'
-   },
-   // 规则配置
-   rules: {
-     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-     'vue/name-property-casing': 'off',
-     'vue/require-prop-types': 'off',
-     'vue/singleline-html-element-content-newline': 'off',
-     "camelcase": 'off',
-     'semi': [
-       'error',
-       'always'
-     ],
-     'semi-spacing': 'error',
-     'vue/max-attributes-per-line': [
-       'error', {
-         'singleline': 2,
-         'multiline': {
-           'max': 1,
-           'allowFirstLine': false
-         }
-       }
-     ],
-     'comma-dangle': [
-       'error',
-       {
-         arrays: 'never',
-         objects: 'never',
-         imports: 'never',
-         exports: 'never',
-         functions: 'ignore'
-       }
-     ],
-     'eqeqeq': [
-       'error',
-       'allow-null'
-     ]
-   }
- };
- 
+  root: true,
+  // 指定脚本运行环境
+  env: {
+    browser: true,
+    node: true
+  },
+  // 基础配置
+  'extends': [
+    '@vue/standard',
+    'plugin:vue/recommended'
+  ],
+  // 指定解析器
+  // babel-eslint：一个对babel解析器的包装，使其能够参与Eslint兼容
+  parserOptions: {
+    parser: 'babel-eslint',
+    // ECMAScript 版本
+    "ecmaVersion": 6,
+    "sourceType": "script",//module
+    // 想使用的额外的语言特性:
+    "ecmaFeatures": {
+      // 允许在全局作用域下使用 return 语句
+      "globalReturn": true,
+      // impliedStric
+      "impliedStrict": true,
+      // 启用 JSX
+      "jsx": true
+    },
+
+    // 规则配置
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'vue/name-property-casing': 'off',
+      'vue/require-prop-types': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      "camelcase": 'off',
+      'semi': [
+        'error',
+        'always'
+      ],
+      'semi-spacing': 'error',
+      'vue/max-attributes-per-line': [
+        'error', {
+          'singleline': 2,
+          'multiline': {
+            'max': 1,
+            'allowFirstLine': false
+          }
+        }
+      ],
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'never',
+          objects: 'never',
+          imports: 'never',
+          exports: 'never',
+          functions: 'ignore'
+        }
+      ],
+      'eqeqeq': [
+        'error',
+        'allow-null'
+      ]
+    }
+  };
